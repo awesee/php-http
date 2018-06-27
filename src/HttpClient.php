1f837f976892919ced0d4c9ad1895b74b09aeb0d
Openset <openset.wang@gmail.com>
2018-06-27 16:00:26 +0800

    Add: getClient

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -2,7 +2,22 @@
 
 namespace Openset;
 
+use GuzzleHttp\Client;
+
 class HttpClient
 {
+    protected static $client;
+
+    public static function getClient()
+    {
+        if (is_null(self::$client)) {
+            self::$client = new Client([
+                // You can set any number of default request options.
+                'timeout' => 30.0,
+            ]);
+        }
+
+        return self::$client;
+    }
 
 }
